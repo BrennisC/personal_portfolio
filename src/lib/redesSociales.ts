0736403a92bd3fdcdc8b1b9691da7870db948831
brenniscastro <brennis.castro@unas.edu.pe>
2025-06-18 07:47:00 -0500

    feat: add social media links and custom global styles for improved UI

---
+++ src/lib/redesSociales.ts
@@ -0,0 +1,11 @@
+export const redesSociales = [
+    {
+        nombre: 'LinkedIn',
+        url: 'https://bit.ly/brennis-benjaminn',
+    },
+    {
+        nombre: 'GitHub',
+        url: 'https://github.com/BrennisC',
+    },
+
+]
